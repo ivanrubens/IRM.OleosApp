18a0c579e1ecd6bc9c4b8a3c78f6559c746e47f1
Ivan R. Martins <ivanrubens@gmail.com>
2023-10-13 23:35:23 -0300

    Remove ignored files

--- src/IRM.OleosApp.Api/Migrations/Configuration.cs
+++ src/IRM.OleosApp.Api/Migrations/Configuration.cs
@@ -5,14 +5,14 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<IRM.OleosApp.Api.Data.OleoAppDBContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<IRM.OleosApp.Api.Data.OleosAppDBContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(IRM.OleosApp.Api.Data.OleoAppDBContext context)
+        protected override void Seed(IRM.OleosApp.Api.Data.OleosAppDBContext context)
         {
             //  This method will be called after migrating to the latest version.
 
